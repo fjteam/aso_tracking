50d410d5a83744056b59dbb13b77f7a5e5f26c94
feiji <neverdie998@163.com>
2016-06-01 06:47:50 +0800

    test

--- test_db.js
+++ test_db.js
@@ -1,6 +1,6 @@
 var db = require('./db');
 
-db.get_app_aso_rank_log(494216511,function(data)
+db.get_app_keyword_dt_rank(494216511,'租租车','2016-6-1 6:00',function(data)
 {
    console.log(data);
 });
