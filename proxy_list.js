6d816cdad13972abb370c91c1e0c3caf2d777c91
feiji <neverdie998@163.com>
2016-06-15 10:26:07 +0800

    distinct keywords

--- proxy_list.js
+++ proxy_list.js
@@ -7,10 +7,12 @@ var _ = require('underscore');
 var proxy_list = []
 
 //proxy_list.push('http://0.0.0.0:80')
+proxy_list.push('http://122.225.107.145:80')
+proxy_list.push('http://123.157.233.138:8003')
 proxy_list.push('http://120.55.160.110:8088')
-proxy_list.push('http://101.81.242.78:8118')
-proxy_list.push('http://183.245.147.26:80')
-proxy_list.push('http://183.203.22.103:8080')
+proxy_list.push('http://183.245.147.37:80')
+proxy_list.push('http://163.177.159.144:81')
+proxy_list.push('http://106.75.128.87:80')
 
 
 
